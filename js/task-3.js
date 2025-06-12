621833e3f191d50e8c818f7a7d15299838651ab3
vahitemre <vahitemrebakioglu@gmail.com>
2025-06-12 17:52:32 +0300

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,5 @@
 function getElementWidth(content, padding, border) {
-  return (
-    Number.parseFloat(content) +
-    2 * Number.parseFloat(padding) +
-    2 * Number.parseFloat(border)
-  );
+  return parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
